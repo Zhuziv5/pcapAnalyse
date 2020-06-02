46b036fbaca411053089bc87b12b752d4f1bd20c
username <zhuzi_szh@163.com>
2020-06-02 23:41:11 +0800

    To read a pcapfile.txt and show it on the screen

--- main.c
+++ main.c
@@ -1,7 +1,22 @@
-#include<stdio.h>
+#include <stdio.h>
 
 int main(void)
 {
-    put("Entry of the app!");
-    
+    puts("****************open the file!************************");
+
+    FILE *pcapFile = fopen("pcap.txt", "r");
+
+    char cprt = 'o';
+    while (1)
+    {
+        cprt = fgetc(pcapFile);
+        if (feof(pcapFile))
+        {
+            break;
+        }
+        printf("%c", cprt);
+    }
+
+    fclose(pcapFile);
+    puts("\n****************file has been closed!****************");
 }
---
+++ pcaptool.h
@@ -0,0 +1,5 @@
+#ifndef __PCAPTOOL_H__
+#define __PCAPTOOL_H__
+#endif
+
+void *readPcapFile();
